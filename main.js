babf10cd1c8ffff50180592a286945efe4e6c108
David Silvério <david.a.silverio@gmail.com>
2019-11-16 20:30:50 +0000

    Added some HTML to the project.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = () => {
     let xhr = new XMLHttpRequest();
-    // Request to open weather map -- API --> 5cccb144e99fcd50583cc21521086247
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=leiria,pt&units=metric&appid=2b2afaf78c88ac55d3e2127d280f0dbc&lang=pt');
+    // Request to open weather map -- API --> nova api key - 5cccb144e99fcd50583cc21521086247   | velha api key - 2b2afaf78c88ac55d3e2127d280f0dbc
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=leiria,pt&units=metric&appid=5cccb144e99fcd50583cc21521086247');
     xhr.onload = () => {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
